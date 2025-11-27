f64d08728965387623fece54ca098b6a94505b34
mafutka <mafutka@gmail.com>
2025-11-27 08:25:38 +0200

    caritem-completed

---
+++ components/CarItem/CarItem.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { Car } from "@/types/types";
+import css from "./CarItem.module.css";
+// import { useFavoriteStore } from "@/lib/store/favoriteStore";
+import { Button } from "../Button/Button";
+
+interface CarItemProps {
+    car: Car;
+}
+
+export default function CarItem({ car }: CarItemProps) {
+    
+    // const toggleFavorite = useFavoriteStore((s) => s.toggleFavorite);
+    // const favorites = useFavoriteStore((s) => s.favorites);
+    // const isFav = favorites.includes(car.id);
+
+    const [city, country] = car.address.split(", ");
+  
+return (
+    <li className={css.card}>
+        <div className={css.imgwrapper}>
+            <Image src={car.img} alt={car.model} className={css.image}></Image>
+       
+            {/* <button
+                className={css.like}
+                onClick={() => toggleFavorite(car.id)}
+                aria-label="Add to favorites"
+            >
+            <Heart
+                fill={isFav ? "#3470FF" : "transparent"}
+                stroke="#3470FF"
+                strokeWidth={2}
+            />
+            </button> */}
+        </div>
+      
+        <div className={css.info}>  
+            <h3 className={css.title}>{car.brand}{" "} 
+                <span className={css.span}>
+                    {car.model}
+                </span>, {car.year} </h3>
+            <p className={css.title}>${car.rentalPrice}</p>
+        </div>
+        <div className={css.details}>
+            <span>{city}</span>
+            <span className={css.dot}> | </span>
+            <span>{country}</span>
+            <span className={css.dot}> | </span>
+            <span>{car.rentalCompany}</span>
+            <span className={css.dot}> | </span>
+            <span>{car.type}</span>
+            <span className={css.dot}> | </span>
+            <span>{car.mileage.toLocaleString()} km</span>
+        </div>
+
+        <Button href={`/catalog/${car.id}`}>Read more</Button>
+    </li>
+  );
+}
+
--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import { api } from "@/app/api/api";
 import { Car, GetCarsResponse } from "@/types/types";
 
 export const getCars = async (page = 1, limit = 12, filters = {}) => {
-  const { data } = await axios.get<GetCarsResponse>("/cars", {
+  const { data } = await api.get<GetCarsResponse>("/cars", {
     params: { page, limit, ...filters },
   });
 
@@ -10,13 +10,14 @@ export const getCars = async (page = 1, limit = 12, filters = {}) => {
 };
 
 export const getCarById = async (id: string) => {
-    const { data } = await axios.get<Car>(`/catalog/${id}`);
+    const { data } = await api.get<Car>(`/cars/${id}`);
     console.log(id);
-    
 
     return data;
    
 };
+
+
 
 
 
